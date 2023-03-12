c26e644af7aea3ad20305ef5bbeffb20554ff450
Mdtcy <tktetb@163.com>
2023-03-12 20:35:34 +0800

    开枪缩放枪口 模仿震动

--- LWShootDemo/Assets/Scripts/Weapons/ProjectileWeapon.cs
+++ LWShootDemo/Assets/Scripts/Weapons/ProjectileWeapon.cs
@@ -7,6 +7,7 @@
  */
 
 #pragma warning disable 0649
+using DG.Tweening;
 using LWShootDemo.Common;
 using LWShootDemo.Entities;
 using LWShootDemo.Managers;
@@ -25,16 +26,21 @@ namespace LWShootDemo.Weapons
         [SerializeField]
         private GameObject pfbProjectile;
 
+        // 粒子特效 闪光 弹壳
         [SerializeField]
         private ParticleSystem fireParticle;
 
         [SerializeField]
+        private Transform model;
+
+        [SerializeField]
         private float fireKnockBackForce = 1;
 
-        // local
-        private Entity       owener;
-        private SoundManager soundManager;
+        // * local
+        private Entity           owener;
+        private SoundManager     soundManager;
         private CameraController cameraController;
+        private Tween            scaleTween;
 
         #endregion
 
@@ -46,21 +52,38 @@ namespace LWShootDemo.Weapons
 
         public override void Init(Entity entity)
         {
-            this.owener = entity;
+            owener = entity;
         }
 
+        /// <summary>
+        /// 使用
+        /// </summary>
         public override void Use()
         {
+            // 随机旋转一下枪口，可以让子弹看起来更随机
             firePoint.localEulerAngles = new Vector3(0f, 0f, Random.Range(-10f, 10f));
             var firePointPos = firePoint.position;
-            Projectile projectile = Instantiate(this.pfbProjectile, firePointPos, firePoint.localRotation)
+
+            // 生成子弹
+            Projectile projectile = Instantiate(pfbProjectile, firePointPos, firePoint.localRotation)
                .GetComponent<Projectile>();
             projectile.Setup(firePoint.rotation);
+
+            // 对持有者产生后坐力
             owener.ApplyKnowBack(0.2f, -transform.up * fireKnockBackForce);
+
+            // 开枪时缩放突出枪口震动
+            scaleTween.Kill();
+            scaleTween = model.DOScale(2, 0.1f).OnComplete(() => model.DOScale(1, 0.1f));
+
+            // 开火粒子特效，包括枪口火焰和弹壳
             fireParticle.Play();
+
+            // 音效
             soundManager.PlaySfx(SoundType.Fire);
-            cameraController.Shake((transform.position - firePointPos).normalized, 0.5f,
-                                                        0.05f);
+
+            // 屏幕震动
+            cameraController.Shake((transform.position - firePointPos).normalized, 0.5f, 0.05f);
         }
 
         #endregion
