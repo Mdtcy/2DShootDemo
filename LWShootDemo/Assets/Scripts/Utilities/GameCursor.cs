1af2387d38a9b882a5e9b816b663fb21119a3402
Mdtcy <tktetb@163.com>
2023-03-12 21:18:54 +0800

    优化游戏光标

--- LWShootDemo/Assets/Scripts/Utilities/GameCursor.cs
+++ LWShootDemo/Assets/Scripts/Utilities/GameCursor.cs
@@ -7,6 +7,7 @@
  */
 
 #pragma warning disable 0649
+using DG.Tweening;
 using LWShootDemo.Managers;
 using UnityEngine;
 
@@ -20,6 +21,29 @@ namespace LWShootDemo.Common
         #region FIELDS
 
         private Camera mainCamera;
+
+        // 最小缩放比例
+        [SerializeField]
+        private float minScale;
+
+        // 最大缩放比例
+        [SerializeField]
+        private float maxScale;
+
+        // 缩放速度
+        [SerializeField]
+        private float speed;
+
+        // 恢复速度
+        [SerializeField]
+        private float recoverSpeed;
+
+        // 鼠标按下时的缩放比例
+        [SerializeField]
+        private Vector3 scaleOnMouseDown;
+
+        // 目标缩放比例
+        private Vector3 targetScale;
 
         #endregion
 
@@ -43,12 +67,30 @@ namespace LWShootDemo.Common
             Cursor.visible = false;
         }
 
+        private float resetSpeed;
+        private float loopSpeed;
+
         private void Update()
         {
-            Cursor.visible = false;
+            // 鼠标按下时，恢复缩放比例为1
+            if (Input.GetMouseButton(0))
+            {
+                targetScale = scaleOnMouseDown;
+            }
+            // 鼠标抬起时，让光标在两个缩放比例之间来回循环
+            else
+            {
+                float t     = Mathf.PingPong(Time.time * speed, 1f);
+                float scale = Mathf.Lerp(minScale, maxScale, t);
+                targetScale = new Vector3(scale, scale, 1f);
+            }
+            transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * recoverSpeed);
 
             Vector2 cursorPos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
             transform.position = cursorPos;
+
+            Cursor.visible = false;
+
         }
 
         #endregion
