17aa680a6845a88ba218edcd8313e618301707b8
God <dunkydd7@gmail.com>
2023-04-27 00:41:29 +0800

    1

--- LWShootDemo/Assets/ZZZ/Mono/Core/Global/GlobalDefine.cs
+++ LWShootDemo/Assets/ZZZ/Mono/Core/Global/GlobalDefine.cs
@@ -10,6 +10,13 @@ namespace ET
         public static ILog ILog;
 
         public static Options Options = new Options();
+        /// <summary>
+        /// 固定间隔的目标FPS
+        /// </summary>
+        public const int FixedUpdateTargetFPS = 30;
+        public const float FixedUpdateTargetDTTime_Float = 1f / FixedUpdateTargetFPS;
+
+        public const long FixedUpdateTargetDTTime_Long = (long) (FixedUpdateTargetDTTime_Float * 1000);
     }
     
     public class Options
