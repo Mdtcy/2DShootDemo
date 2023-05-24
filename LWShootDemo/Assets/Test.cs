667ce8e662af71df7e9778fabce3546f72bde551
God <dunkydd7@gmail.com>
2023-05-24 21:07:51 +0800

    更新TestUseCase : Event

--- LWShootDemo/Assets/Test.cs
+++ LWShootDemo/Assets/Test.cs
@@ -1,8 +1,11 @@
+using System;
 using GameFramework;
+using GameFramework.Event;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityGameFramework.Runtime;
+using Zenject;
 
 namespace DefaultNamespace
 {
@@ -30,6 +33,53 @@ namespace DefaultNamespace
             Log.Warning("Warning");
             Log.Error("Error");
             Log.Fatal("Fatal");
+        }
+
+        [Inject]
+        private IEventManager _eventManager;
+        
+        [Button]
+        public void TestSurcribe()
+        {
+            _eventManager.Subscribe(TestEvent.EventId, OnTestEvent);
+        }
+        
+        [Button]
+        public void TestUnSurcribe()
+        {
+            _eventManager.Unsubscribe(TestEvent.EventId, OnTestEvent);
+        }
+        
+        [Button]
+        public void TestFireTestEvent()
+        {
+            _eventManager.Fire(this, TestEvent.Create());
+        }
+
+        private void OnTestEvent(object sender, GameEventArgs e)
+        {
+            Log.Info("OnTestEvent");
+        }
+
+        public class TestEvent:GameEventArgs
+        {
+            public static TestEvent Create()
+            {
+                return ReferencePool.Acquire<TestEvent>();
+            }
+
+            public override void Clear()
+            {
+                Log.Info("OnTEstEventClear");
+            }
+
+            /// <summary>
+            /// 显示物体成功事件编号。
+            /// </summary>
+            public static readonly int EventId = typeof(TestEvent).GetHashCode();
+
+            public override int Id => EventId;
+            
         }
 
         public class AClass : IReference
